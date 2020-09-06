e6bce0c62804773b672e7afee6ed1cfb3187b4cf
sunpeer <1031826952@qq.com>
2020-09-06 19:51:33 +0800

    增删改查基础语句完善

--- CRUD.js
+++ CRUD.js
@@ -3,15 +3,21 @@ function insertOpr(table_name,key_values,connection,cb){
 }
 
 function updateOpr(table_name,key_values,index,connection,cb){
-    connection.query(`update table ${table_name} set ? where id=${index}`,key_values,cb)
+    connection.query(`update ${table_name} set ? where id=${index}`,key_values,cb)
 }
 
-function queryOpr(table_name,query_key,index,cb){
+function queryByIndex(table_name,query_key,index,connection,cb){
     connection.query(`select ? from ${table_name} where id=${index}`,query_key,cb)
 }
+
+function queryBycondition(data,table_name,condition,connection,cb){
+    connection.query(`select ${data} from ${table_name} ${condition}`,cb) 
+}
+
 
 module.exports={
     insertOpr,
     updateOpr,
-    queryOpr
+    queryByIndex,
+    queryBycondition
 }
